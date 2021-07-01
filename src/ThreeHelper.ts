850f0da41fb184089db2e58234fd9d1afd9b5545
Duc Nguyen <tom@ducng.dev>
2021-07-01 16:58:06 +1200

    Move assets to 'assets' folder

--- src/ThreeHelper.ts
+++ src/ThreeHelper.ts
@@ -36,7 +36,7 @@ export async function Init() {
 export async function SetupRoom() {
     let loader = new (await import("three/examples/jsm/loaders/GLTFLoader.js")).GLTFLoader();
     
-    loader.load('/3DModels/Bedroom.glb', function (gltf) {
+    loader.load('./assets/3DModels/Bedroom.glb', function (gltf) {
         bedroomModel = new Model();
         bedroomModel.InitFromGLTF(gltf);
 
@@ -49,7 +49,7 @@ export async function SetupRoom() {
 export async function SetupHuman(onLoaded?: Function) {
     let loader = new (await import("three/examples/jsm/loaders/GLTFLoader.js")).GLTFLoader();
     
-    loader.load('/3DModels/human.glb', function (gltf) {
+    loader.load('./assets/3DModels/human.glb', function (gltf) {
         humanModel = new Model();
         humanModel.InitFromGLTF(gltf);
 
